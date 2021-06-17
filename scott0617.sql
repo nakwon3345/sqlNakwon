2be7b94d2cabdecce78258a178d081aa5c9b0c01
nakwon3345 <84373983+nakwon3345@users.noreply.github.com>
2021-06-17 22:46:17 +0900

    0617sql

---
+++ scott0617.sql
@@ -0,0 +1,7 @@
+select * from notice;
+insert into notice values('1', '주말과제','회원도서관리 DB 설계' ,'user05', '2020.11.11', 0);
+insert into notice values('2', '형상관리','형상관리의 소개' ,'user04', '2020.12.25', 5);
+insert into notice values('3', '주말과제','화면정의서' ,'user05', '2021.02.14', 0);
+insert into notice values('4', '과제제출','시간엄수' ,'user05', '2021.03.01', 15);
+insert into notice values('5', 'WEB참고','www.w3schools.com' ,'user01', '2021.05.26', 5);
+commit;
